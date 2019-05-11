1330f6dfec7e61dabe6ac63495f50a0c598f456e
pravin ranjan <pravinranjan10@gmail.com>
2019-05-11 16:50:45 +0530

    This is just for test

--- contrib/drivers/filesharedrivers/drivers.go
+++ contrib/drivers/filesharedrivers/drivers.go
@@ -21,7 +21,8 @@ plugin, just modify Init() and Clean() method.
 package filesharedrivers
 
 import (
-	"github.com/opensds/opensds/contrib/drivers/filesharedrivers/nfs"
+	"fmt"
+	nfs "github.com/opensds/opensds/contrib/drivers/filesharedrivers/nfs"
 	"github.com/opensds/opensds/contrib/drivers/utils/config"
 	"github.com/opensds/opensds/pkg/model"
 	pb "github.com/opensds/opensds/pkg/model/proto"
@@ -43,13 +44,16 @@ type FileShareDriver interface {
 
 // Init
 func Init(resourceType string) FileShareDriver {
+	fmt.Println("resourceType ...",resourceType)
 	var f FileShareDriver
 	switch resourceType {
 	case config.NFSDriverType:
-		f = &nfs.NFSDriver{}
+		fmt.Println("IN nfs driver...")
+		f = &nfs.Driver{}
 		break
 	default:
-		f = &sample.Driver{}
+		fmt.Println("IN nfs-sample driver...")
+		f = &nfs.Driver{}
 		break
 	}
 	return f
@@ -59,7 +63,7 @@ func Init(resourceType string) FileShareDriver {
 func Clean(f FileShareDriver) FileShareDriver {
 	// Execute different clean operations according to the FileShareDriver type.
 	switch f.(type) {
-	case *nfs.NFSDriver:
+	case *nfs.Driver:
 		break
 	case *sample.Driver:
 		break
